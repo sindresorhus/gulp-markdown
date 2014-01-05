6683874cbf90c5bda5aa9bc17cb6b9d4320bca17
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-05 01:14:57 +0100

    use map-stream

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
-var es = require('event-stream');
 var gutil = require('gulp-util');
+var map = require('map-stream');
 var marked = require('marked');
 
 module.exports = function (options) {
-	return es.map(function (file, cb) {
+	return map(function (file, cb) {
 		marked(file.contents.toString(), options, function (err, data) {
 			if (err) {
 				return cb(new Error('gulp-markdown: ' + err));
