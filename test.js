d1fff1279fdae584363e38fe750521e3dd33be88
Sindre Sorhus <sindresorhus@gmail.com>
2018-08-09 02:09:32 +0700

    Require Node.js 6 and update `marked`
    Closes #14

--- test.js
+++ test.js
@@ -1,26 +1,26 @@
 import test from 'ava';
 import Vinyl from 'vinyl';
-import m from '.';
+import pEvent from 'p-event';
+import markdown from '.';
 
-test.cb('compiles Markdown to HTML', t => {
-	const stream = m();
-
-	stream.once('data', file => {
-		t.is(file.relative, 'fixture.html');
-		t.is(file.contents.toString(), '<p><em>foo</em></p>\n');
-	});
-
-	stream.on('end', t.end);
+test('compiles Markdown to HTML', async t => {
+	const stream = markdown();
+	const dataPromise = pEvent(stream, 'data');
 
 	stream.end(new Vinyl({
 		path: 'fixture.md',
 		contents: Buffer.from('*foo*')
 	}));
+
+	const file = await dataPromise;
+
+	t.is(file.relative, 'fixture.html');
+	t.is(file.contents.toString(), '<p><em>foo</em></p>\n');
 });
 
 test('exposes the marked object', t => {
-	t.truthy(m.marked);
-	t.truthy(m.marked.Renderer);
-	t.truthy(m.marked.lexer);
-	t.truthy(m.marked.parser);
+	t.truthy(markdown.marked);
+	t.truthy(markdown.marked.Renderer);
+	t.truthy(markdown.marked.lexer);
+	t.truthy(markdown.marked.parser);
 });
