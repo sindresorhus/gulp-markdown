11173968688b69961c56e35c21195c0730772d98
Sindre Sorhus <sindresorhus@gmail.com>
2019-04-11 15:51:13 +0700

    Require Node.js 8

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
+const {promisify} = require('util');
 const through = require('through2');
 const marked = require('marked');
 const PluginError = require('plugin-error');
 
+const pMarked = promisify(marked);
+
 module.exports = options => {
-	return through.obj((file, encoding, callback) => {
+	return through.obj(async (file, encoding, callback) => {
 		if (file.isNull()) {
 			callback(null, file);
 			return;
@@ -15,17 +18,14 @@ module.exports = options => {
 			return;
 		}
 
-		marked(file.contents.toString(), options, (error, data) => {
-			if (error) {
-				callback(new PluginError('gulp-markdown', error, {fileName: file.path}));
-				return;
-			}
-
+		try {
+			const data = await pMarked(file.contents.toString(), options);
 			file.contents = Buffer.from(data);
 			file.extname = '.html';
-
 			callback(null, file);
-		});
+		} catch (error) {
+			callback(new PluginError('gulp-markdown', error, {fileName: file.path}));
+		}
 	});
 };
 
